e62855b1d78d0a561695d7541d0b26b1a78afb75
salazarroman429-ui <salazarroman429@gmail.com>
2026-01-31 06:07:01 -0500

    Update app.js

--- tienda/app.js
+++ tienda/app.js
@@ -1,5 +1,5 @@
 // app.js - Versión completa actualizada (tienda)
-const API_URL = 'https://proveedor-tienda-api.onrender.com/api';
+const API_URL = 'https://proveedor-api-salazar.onrender.com/api';
 let deferredPrompt;
 let isAuthenticated = false;
 let currentStore = null;
